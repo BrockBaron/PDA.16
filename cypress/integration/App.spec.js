7db2b059fc48270f75b54b1dbe0a22560ceb862e
BrockBaron <Nslekkas@gmail.com>
2021-04-25 15:19:02 +0100

    UI test 4 setup range of outputs

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -20,9 +20,58 @@ describe("Calculator", () => {
     cy.get('#number1').click();
     cy.get('#operator_add').click();
     cy.get('#number2').click();
-    cy.get('#equals').click();
+    cy.get('#operator_equals').click();
     
     cy.get('.display').should('contain', '3')
   })
+
+  xit('should display result of arithmetic operation equaling negative number', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-1')
+  })
+
+  xit('should display result of arithmetic operation equaling float number', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_divided').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.6')
+  })
+
+  xit('should display result of arithmetic operation equaling larg number greater than 10 sig fig', () => {
+    //14 9's by 8 9s = 9.9999998999999e+21
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '9.9999998999999e+21')
+  })
+
+  xit('should display apropriate output in even of exceptional circumstance dividing by 0', () => {
+    // the code should diplay Error: Division by zero, when numner is divide by zero.
+    
+    cy.get('.display').should('contain', '')
 
 })
